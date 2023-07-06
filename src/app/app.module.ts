a3179c4ea13ff30a90046400a97ab5e0f37d0b78
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-07-06 03:10:25 +0300

    import all created components into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NotFoundedComponent } from './not-founded/not-founded.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { BannerComponent } from './banner/banner.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundedComponent,
+    HomeComponent,
+    ContactUsComponent,
+    BannerComponent,
+    StatisticsComponent,
+    PartnersComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
